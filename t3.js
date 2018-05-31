20c97ba2a7257c18e14c7fbea63e36b954aacfcb
sidroc01 <sidroc01@gmail.com>
2018-05-31 15:27:35 +0300

    Create t3.js

---
+++ t3.js
@@ -0,0 +1,14 @@
+/*eslint no-debugger: "error"*/
+
+function isTruthy1(x) {
+    
+    return Boolean(x);
+}
+
+function isTruthy2(x) {
+    
+    return Boolean(x);
+}
+
+isTruthy1("True");
+isTruthy2("False");
