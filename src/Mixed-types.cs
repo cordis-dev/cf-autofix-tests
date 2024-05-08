ade9e22a765ddba48c412e132ce5a22817bdb3ae
cordis-dev <darius@adroiti.com>
2024-05-08 12:32:34 +0300

    Create Mixed-types.cs

---
+++ src/Mixed-types.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutofixTests
+{
+    internal class MixedTypes
+    {
+        public static void Test()
+        {
+            int pid = Process.GetCurrentProcess().Id;
+        }
+
+
+    }
+}
