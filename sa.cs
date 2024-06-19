aa5d765582a350860fd293b915443504c5bb7573
cordis-dev <darius@adroiti.com>
2024-06-19 15:41:15 +0300

    Create sa.cs

---
+++ sa.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutofixTests
+{
+    internal class MixedTypes
+    {
+        public static void Test()
+        {
+            int pid = Environment.ProcessId;
+        }
+		
+    }
+}
